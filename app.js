396d7fe9be44d536254d75b0f988fe04c4a1199d
Muahammad Waqas <mwaqas6001@gmail.com>
2023-08-31 01:47:23 +0500

    add more cards

--- app.js
+++ app.js
@@ -97,16 +97,177 @@ let cities = [
     }
 ];
 
+let cuisines = [
+    {
+        image: "./imagesCusin/fast.jpeg",
+        city: "FastFood"
+    },
+    {
+        image: "./imagesCusin/pizza.jpg",
+        city: "Pizza"
+    },
+    {
+        image: "./imagesCusin/desert.jpeg",
+        city: "Dessert&Bakes"
+    },
+    {
+        image: "./imagesCusin/pakistani.jpg",
+        city: "Pakistani"
+    },
+    {
+        image: "./imagesCusin/beawerges.webp",
+        city: "Beverages"
+    },
+    {
+        image: "./imagesCusin/Burger.jpg",
+        city: "Burger"
+    },
+    {
+        image: "./imagesCusin/american.jpeg",
+        city: "American"
+    },
+    {
+        image: "./imagesCusin/chiness.jpg",
+        city: "Chiness"
+    },
+    {
+        image: "./imagesCusin/arabic.jpg",
+        city: "Arabic"
+    },
+    {
+        image: "./imagesCusin/afghani.jpg",
+        city: "Afghani"
+    },
+    {
+        image: "./imagesCusin/italian.jpg",
+        city: "Italian"
+    },
+    {
+        image: "./imagesCusin/turkish.jpg",
+        city: "Turkish"
+    },
+    {
+        image: "./imagesCusin/thai.jpg",
+        city: "Thai Food"
+    },
+    {
+        image: "./imagesCusin/steet.jpg",
+        city: "Street Food"
+    },
+    {
+        image: "./imagesCusin/pan_asia.jpeg",
+        city: "Pan Asia"
+    },
+    {
+        image: "./imagesCusin/lebanese.jpg",
+        city: "Lebanese"
+    },
+   
+];
 
-let cityCardsContainor =document.querySelector(".cityCardsContainor");
+
+
+let recipes = [
+    {
+        image: "./imagesRecipe/Rbbq.jpg",
+        city: "BBQ"
+    },
+    {
+        image: "./imagesRecipe/Rbriyani.avif",
+        city: "Briyani"
+    },
+    {
+        image: "./imagesRecipe/Rbreakfast.jpg",
+        city: "BreakFast"
+    },
+    {
+        image: "./imagesRecipe/Rpasta.jpg",
+        city: "Pasta"
+    },
+    {
+        image: "./imagesRecipe/Rdrink.jpg",
+        city: "Drinks"
+    },
+    {
+        image: "./imagesRecipe/Rsnack.jpeg",
+        city: "Snack"
+    },
+    {
+        image: "./imagesRecipe/Rsoup.avif",
+        city: "Soup"
+    },
+    {
+        image: "./imagesRecipe/Rcake.avif",
+        city: "Cakes"
+    },
+    {
+        image: "./imagesRecipe/Rdessert.jpg",
+        city: "Dessert & Sweet"
+    },
+    {
+        image: "./imagesRecipe/Rvegtarian.avif",
+        city: "Vegterian"
+    },
+    {
+        image: "./imagesRecipe/Rsalad.jpg",
+        city: "Salad"
+    },
+    {
+        image: "./imagesRecipe/Rsauce.jpg",
+        city: "Sauce"
+    },
+
+];
+
+
+
+let cityCardsContainor =document.getElementById("cityCardsContainor");
 
 cities.forEach((data,index)=>{
     cityCardsContainor.innerHTML +=`
-    <div class="cityCard col-lg-2 col-md-4 col-sm-12 col-12" >
+    <div class="cityCard  .col-xxl-12  .col-xl-12 col-lg-2 col-md-4 col-sm-12 col-12" >
     <div class="card">
         <div class="image-container">
             
-            <img src="${data.image}" alt="Card Image">
+            <img src="${data.image}" alt="Card Image" style="padding: 0;">
+        </div>
+        <div class="content">
+          <h2>${data.city}</h2>
+        </div>
+        <button class="btn ">Discover</button>
+      </div>
+</div>
+    `
+})
+
+
+let cuisinesCardsContainor =document.getElementById("cuisinesCardsContainor");
+
+cuisines.forEach((data,index)=>{
+    cuisinesCardsContainor.innerHTML +=`
+    <div class="cityCard  .col-xxl-12  .col-xl-12 col-lg-3 col-md-6 col-sm-12 col-12" >
+    <div class="card">
+        <div class="image-container">
+            
+            <img src="${data.image}" alt="Card Image" ">
+        </div>
+        <div class="content">
+          <h2>${data.city}</h2>
+        </div>
+        <button class="btn ">Discover</button>
+      </div>
+</div>
+    `
+})
+let recipeCardsContainor =document.getElementById("recipeCardsContainor");
+
+recipes.forEach((data,index)=>{
+    recipeCardsContainor.innerHTML +=`
+    <div class="cityCard  .col-xxl-12  .col-xl-12 col-lg-2 col-md-4 col-sm-12 col-12" >
+    <div class="card">
+        <div class="image-container">
+            
+            <img src="${data.image}" alt="Card Image" ">
         </div>
         <div class="content">
           <h2>${data.city}</h2>
